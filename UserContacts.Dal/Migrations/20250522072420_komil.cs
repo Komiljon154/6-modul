30688c7e0348cb72e0ade438bbfe23747fed3987
Komiljon154 <komiljonk154@gmail.com>
2025-05-22 12:28:50 +0500

    _

---
+++ UserContacts.Dal/Migrations/20250522072420_komil.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserContacts.Dal.Migrations
+{
+    /// <inheritdoc />
+    public partial class komil : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
